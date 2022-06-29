0d35b6067f0daeab44836bc1eb95b29e8228533f
ganesh chachriya <g.chachriya11@gmail.com>
2022-06-29 16:10:15 +0530

    added blod forn to all items

--- dom.js
+++ dom.js
@@ -1,15 +1,15 @@
 
 // GET THE ELEMENT BY ID
-// console.dir(document)
+console.dir(document)
 // examine the document object 
-// console.log(document.forms);
-//  console.log(document.links);
-//  console.log(document.images);
-//  console.log(document.getElementById("header-title"));
-//  var headerTitle = document.getElementById("header-title");
-// console.log(headerTitle);
-// headerTitle.textContent = "Hello";
-//  headerTitle.innerText = "Goodbye";
+console.log(document.forms);
+ console.log(document.links);
+ console.log(document.images);
+ console.log(document.getElementById("header-title"));
+ var headerTitle = document.getElementById("header-title");
+console.log(headerTitle);
+headerTitle.textContent = "Hello";
+ headerTitle.innerText = "Goodbye";
 var header = document.getElementById('main-header');
 header.style.borderBottom = 'solid 3px #000';
 
@@ -23,3 +23,7 @@ items[1].textContent = 'Hello 2';
 items[1].style.fontWeight = 'bold';
 items[1].style.backgroundColor = 'yellow';
 items[1].style.color = "green";
+items[2].style.backgroundColor = 'green'
+for(var i=0; i<items.length; i++) {
+    items[i].style.fontWeight = 'bold';
+}
