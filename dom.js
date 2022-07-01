b1831a4445cbf036dd46a4947ff30da4c906eb87
ganesh chachriya <g.chachriya11@gmail.com>
2022-07-01 12:25:30 +0530

    lerned how to use query selector and query selector all

--- dom.js
+++ dom.js
@@ -1,17 +1,17 @@
 
 // GET THE ELEMENT BY ID
-console.dir(document)
-// examine the document object 
-console.log(document.forms);
- console.log(document.links);
- console.log(document.images);
- console.log(document.getElementById("header-title"));
- var headerTitle = document.getElementById("header-title");
-console.log(headerTitle);
-headerTitle.textContent = "Hello";
- headerTitle.innerText = "Goodbye";
-var header = document.getElementById('main-header');
-header.style.borderBottom = 'solid 3px #000';
+// console.dir(document)
+// // examine the document object 
+// console.log(document.forms);
+//  console.log(document.links);
+//  console.log(document.images);
+//  console.log(document.getElementById("header-title"));
+//  var headerTitle = document.getElementById("header-title");
+// console.log(headerTitle);
+// headerTitle.textContent = "Hello";
+//  headerTitle.innerText = "Goodbye";
+// var header = document.getElementById('main-header');
+// header.style.borderBottom = 'solid 3px #000';
 
 
 
@@ -29,14 +29,45 @@ header.style.borderBottom = 'solid 3px #000';
 // }
 
 //  GET ELEMENT BY TAG NAME
-var li = document.getElementsByClassName('li');
-console.log(li);
-console.log(items[1]);
-li[1].textContent = 'Hello 2';
-li[1].style.fontWeight = 'bold';
-li[1].style.backgroundColor = 'yellow';
-li[1].style.color = "green";
-li[2].style.backgroundColor = 'green'
-for(var i=0; i<items.length; i++) {
-    items[i].style.fontWeight = 'bold';
-}
+// var li = document.getElementsByClassName('li');
+// console.log(li);
+// console.log(items[1]);
+// li[1].textContent = 'Hello 2';
+// li[1].style.fontWeight = 'bold';
+// li[1].style.backgroundColor = 'yellow';
+// li[1].style.color = "green";
+// li[2].style.backgroundColor = 'green'
+// for(var i=0; i<items.length; i++) {
+//     items[i].style.fontWeight = 'bold';
+// }
+
+// GET ELEMENT BY QUERYSELECTER
+
+// var header = document.querySelector('#main-header');
+//  header.style.borderBottom = 'solid 4px #ccc';
+//  var input = document.querySelector('input');
+//  input.value = 'Hello World';
+//  var Submit = document.querySelector('input[type="Submit"]');
+//  Submit.value = 'Send';
+//  var item = document.querySelector('.list-group-item');
+//  item.style.color = 'red';
+//  var lastItem = document.querySelector('.list-group-item:last-child');
+//  lastItem.style.color = 'blue';
+//  var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+//  secondItem.style.color = 'coral';
+
+//  QUERY SELECTORALL
+var titles = document.querySelectorAll('.title');
+ console.log(titles);
+ titles[0].textContent = 'Hello';
+ var item = document.querySelectorAll('li');
+ item[1].style.color = 'green';
+ var odd = document.querySelectorAll('li:nth-Child(odd)'); 
+ var even = document.querySelectorAll('li:nth-child(even)');
+ for(var i=0; i<odd.length; i++) {
+     odd[i].style.background = 'green';
+     even[i].style.background = '#ccc';
+ }
+
+
+
